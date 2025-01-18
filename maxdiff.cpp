539bab009b57634c03ea0e9a1f8dcf5f9b55da76
sourav <mailsourav014@gmail.com>
2025-01-19 00:09:20 +0530

    maxidff

---
+++ maxdiff.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> input = {1, 2, 3, 4};
+    int sum;
+    int max = abs(input[0] - input[input.size() - 1]);
+    for (int i = 0; i < input.size() - 1; i++)
+    {
+        sum = abs(input[i] - input[i + 1]);
+        if (sum > max)
+        {
+            max = sum;
+        }
+    }
+    cout << max;
+}
