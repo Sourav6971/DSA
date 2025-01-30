e4191a20afc5bcbd8bf9accb26e13a142c456a46
sourav <mailsourav014@gmail.com>
2025-01-30 23:40:59 +0530

    learned hashing

--- containsDuplicates.cpp
+++ containsDuplicates.cpp
@@ -1,9 +1,22 @@
 #include <bits/stdc++.h>
+// #include<unordered_set>
+
 using namespace std;
 int main()
 {
     vector<int> array = {2, 3, 4, 5, 6, 7, 7, 7, 3};
     unordered_set<int> myset;
+    /* available functions:-
+    insert(value)
+    erase(value)
+    find(value) returns the value
+    count(value) returns 1 if value exists
+    size() returns the size of the set
+    clear() clears the set
+    empty() returns 1 if the set is empty
+
+
+    */
     for (int i = 0; i < array.size(); i++)
     {
         if (myset.count(array[i]))
